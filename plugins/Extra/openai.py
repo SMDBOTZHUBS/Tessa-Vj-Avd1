daf661ed1b3986d865780f801bb738e35da8b138
SMDBOTZHUBS <154757552+SMDBOTZHUBS@users.noreply.github.com>
2024-01-02 22:30:44 +0530

    Add files via upload

--- plugins/Extra/openai.py
+++ plugins/Extra/openai.py
@@ -1,14 +1,28 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+from info import S_GROUP, OPENAI_API
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import openai
 
-from pyrogram import Client, filters
-from plugins.Extra.engine import ask_ai
+openai.api_key = OPENAI_API
 
-
-@Client.on_message(filters.command('openai'))
-async def openai_ask(client, message):
-    if len(message.command) == 1:
-       return await message.reply_text("Give an input!")
-    m = await message.reply_text("👀")
-    await ask_ai(client, m, message)
+@Client.on_message(filters.command("openai"))
+async def openai(client, message):
+    if message.chat.id != S_GROUP:
+        btn = [[
+            InlineKeyboardButton('Support Group', url="https://telegram.me/BotszSupport")
+        ]]
+        return await message.reply("This command only working in support group.", reply_markup=InlineKeyboardMarkup(btn))
+    try:
+        text = message.text.split(" ", 1)[1]
+    except:
+        return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
+    msg = await message.reply("Searching...")
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=text,
+            max_tokens=2000
+        )
+        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].text}</code>")
+    except Exception as e:
+        await msg.edit(f'Error - <code>{e}</code>')
